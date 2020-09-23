7ac33ace41c18d3f64f1ef223c48a200c50fe7fa
Rasha <r.alamleh@gmail.com>
2020-09-23 22:18:47 +0300

    Adding some JS Function

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 
-// $(function() {
-// var body = $('body');
-// var backgrounds = ["url('Kids-1.jpg')","url('Kids-2.jpg')","url('Kids-3.jpg')"];
-// var i = 0;
-// setInterval(function() {
-//       body.css('background',backgrounds[i]);
-//       i = i + 1;
-//       if (i === backgrounds.length) {
-//         i =  0;
-//       }
-// }, 5000);
-// });
+var body = $('body');
+var backgrounds = ["url('Kids-1.jpg')","url('Kids-2.jpg')"];
+var i = 0;
+setInterval(function() {
+      body.css('background',backgrounds[i]);
+      i = i + 1;
+      if (i === backgrounds.length) {
+        i =  0;
+      }
+}, 5000);
 
+
+
+
+// $('#btn2').click(function(){
+// 	$('#about').append('<li>Hello</li');
+// })
 
 
   
