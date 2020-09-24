b03d0ffb5e1f4ca07412abb74b9032241887d6af
Rasha <r.alamleh@gmail.com>
2020-09-24 10:39:53 +0300

    Final

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
+$(function(){
 
-var body = $('body');
-var backgrounds = ["url('Kids-1.jpg')","url('Kids-2.jpg')"];
-var i = 0;
-setInterval(function() {
-      body.css('background',backgrounds[i]);
+ var img = $('.container');
+ var backgrounds = [$('#img1') ,$('#img2')];
+ var i = 0;
+ setInterval(function() {
+      img.css('background',backgrounds[i]);
       i = i + 1;
       if (i === backgrounds.length) {
         i =  0;
       }
-}, 5000);
+ }, 5000);
+
+})
 
 
 
 
-// $('#btn2').click(function(){
-// 	$('#about').append('<li>Hello</li');
-// })
+
 
 
   
